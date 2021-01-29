d1f9aef2e2e040d5227f2ebb7ec22c8be996f464
rebeckky <bdicroteau@gmail.com>
2021-01-29 11:49:23 -0800

    Add methods to avoid walls, helper methods to convert directions

---
+++ global_variables.py
@@ -0,0 +1,10 @@
+
+
+# board height and width minimum
+BOARD_MINIMUM_X = 0
+BOARD_MINIMUM_Y = 0
+
+# board height and width maximum
+BOARD_MAXIMUM_X = 0
+BOARD_MAXIMUM_Y = 0
+
---
+++ strategies.py
@@ -0,0 +1,45 @@
+
+def convert_coords_to_direction(current_head, x, y):
+
+    if x - current_head["x"] > 0:
+        result = "right"
+    elif x - current_head["x"] < 0:
+        result = "down" # TODO: check logic
+    else:
+        if y - current_head["y"] > 0:
+            result = "up"
+        else:
+            result = "down"
+    
+    return result
+
+def convert_direction_to_coords(current_head, next_move):
+
+    future_head = current_head
+    print(f"before move: {future_head}")
+    if next_move == "right":
+        future_head["x"] = current_head["x"] + 1
+    elif next_move == "left":
+        future_head["x"] = current_head["x"] - 1
+    elif next_move == "up":
+        future_head["y"] = current_head["y"] + 1
+    elif next_move == "down":
+        future_head["y"] = current_head["y"] - 1
+
+    return future_head
+
+
+def avoid_walls(head, move):
+    ''' Return true if the move coords will avoid hitting
+        a wall, false if it will cause the snake to hit the wall
+    '''
+    future_head = convert_direction_to_coords(head, move)
+    print(future_head)
+    if future_head["x"] < 0 or future_head["y"] < 0:
+        result = False
+    elif future_head["x"] > global_variables.BOARD_MAXIMUM_X or future_head["y"] > global_variables.BOARD_MAXIMUM_Y:
+        result = False
+    else:
+        result = True
+    
+    return result
